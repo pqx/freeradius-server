f5828b9dd5e8a09d1cf1c9fedcf94af7695685a1
Sam Hartman <hartmans@debian.org>
2013-04-10 17:49:04 -0400

    Remove hidden structs

--- src/modules/rlm_realm/trustrouter_integ.h
+++ src/modules/rlm_realm/trustrouter_integ.h
@@ -4,13 +4,7 @@
 
 #include <freeradius-devel/radiusd.h>
 #include <freeradius-devel/modules.h>
-#include <trust_router/tid.h>
 
-struct resp_opaque {
-  REALM *output_realm;
-  TID_RC result;
-  char err_msg[1024];
-};          
  
 REALM *tr_query_realm(const char *q_realm,
 		      const char  *q_community,
